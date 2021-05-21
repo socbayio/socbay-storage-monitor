3636f1e22541967f7efee7ba99e022dd460cb077
DESKTOP-ACLUKV6\trong <trongnghia.nguyen@outlook.com>
2021-05-21 21:29:38 +0200

    Remove files in block

--- task/checkBlock.js
+++ task/checkBlock.js
@@ -5,12 +5,28 @@ const { globSource, create, CID } = IpfsHttpClient;
 const { ipfsTimeout } = require('../ipfsconfig');
 
 
+async function removeIfExist(ipfs, cid) {
+    const cidObj = new CID(cid);
+    let existed = false;
+    for await (const pin of ipfs.pin.ls({
+        paths: cidObj,
+        types: 'recursive'
+    })) {
+        if (cidObj.equals(pin.cid)) existed = true;
+    }
+    if (existed) {
+        await ipfs.pin.rm(cidObj);
+        return true;
+    }
+    return false;
+}
+
 module.exports = async (req, res, next) => {
     const ipfs = create({
         timeout: ipfsTimeout
     });
 
-    const uploadBlockFound = await uploadBlock.find().select('_id uploadedToNetwork CID');
+    const uploadBlockFound = await uploadBlock.find().select('_id uploadedToNetwork CID filesInfo');
     for (let blockCount = 0; blockCount < uploadBlockFound.length; blockCount++){
         if (uploadBlockFound[blockCount].CID) {
             const statusObj = await status(uploadBlockFound[blockCount].CID);
@@ -26,18 +42,13 @@ module.exports = async (req, res, next) => {
                     replicas: statusObj[0].reported_replica_count,
                 }
                 if (!uploadBlockFound[blockCount].removedFromLocalIpfs){
-                    const cidObj = new CID(uploadBlockFound[blockCount].CID);
-                    let existed = false;
-                    for await (const pin of ipfs.pin.ls({
-                        paths: cidObj,
-                        types: 'recursive'
-                    })) {
-                        if (cidObj.equals(pin.cid)) existed = true;
-                    }
-                    if (existed) {
-                        await ipfs.pin.rm(cidObj);
+                    const rmStatus = await removeIfExist(ipfs, uploadBlockFound[blockCount].CID);
+                    if (rmStatus) {
                         objToUpload.removedFromLocalIpfs = true;
                     }
+                }
+                for (let fileCount = 0; fileCount < uploadBlockFound[blockCount].filesInfo.length; fileCount++) {
+                    await removeIfExist(ipfs, uploadBlockFound[blockCount].filesInfo[fileCount].CID)
                 }
             }
             uploadBlock.findByIdAndUpdate(uploadBlockFound[blockCount]._id, objToUpload)
